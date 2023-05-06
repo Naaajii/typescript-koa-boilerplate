585172f6e2b3fef8014e5715bd9a5f071bcc832f
Naaajii <vcmpbhai@gmail.com>
2023-05-07 00:16:31 +0500

    chore: new packages for koa

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
 import Koa from "koa";
 
+/* 
+// If you decide to Koa Router after cloning
+import Router from "@koa/router"; 
+*/
+
+/*
+// If you want to Koa Bodyparser after cloning
+import bodyparser from "koa-bodyparser";
+*/
+
+/*
+// If you want to Koa Logger after cloning
+import logger from "koa-logger";
+*/
+
 const app = new Koa();
 
+/* 
+// If you want to Koa Bodyparser after cloning
+app.use(bodyParser()) 
+*/
+
+/* 
+// If you want to Koa Logger after cloning
+app.use(KoaLogger()); 
+*/
+
+/* // If you decide to Koa Router after cloning
+const router: Router = new Router();
+router.get('/hello', (ctx: Koa.Context) => ctx.body = "Hello Koa!");
+app.use(router.routes()).use(router.allowedMethods()); 
+ */
+
 app.listen(
-    process.env.PORT || 6000,
+    3300,
     () => {
         console.log("Koa Started!");
     }
